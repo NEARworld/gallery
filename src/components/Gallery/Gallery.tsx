0b78924f5e7c1684ae720dd3b5598830b644c0b4
NEARworld <artemismars2@outlook.com>
2022-07-21 23:01:38 +0900

    chore: removed unnecessary modules and api calls

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,21 +1,6 @@
 import './Gallery.scss'
-import { createApi } from 'unsplash-js';
-import { useEffect } from 'react';
-
-// temp
-const unsplash = createApi({
-    accessKey: process.env.REACT_APP_API_ACCESS_KEY!
-})
 
 export default function Gallery() {
-
-    useEffect(() => {
-        unsplash.photos.getRandom({
-            count: 30
-        }).then(res => console.log(res.response)
-        ).catch(err => console.log(err)
-        )
-    }, [])
 
     function randomRGB(): string {
         return `rgb(${Math.floor(Math.random() * 255)}, 
